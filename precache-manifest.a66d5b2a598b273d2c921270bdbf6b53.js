5b7f8e3cae605e3278d660a89898b855c78bf386
Serhii Krasnovidov <jenero77@gmail.com>
2020-08-12 20:21:02 +0300

    Updates

---
+++ precache-manifest.a66d5b2a598b273d2c921270bdbf6b53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5a268d85a71750d9e85daf47000de4",
+    "url": "/cardzapp/index.html"
+  },
+  {
+    "revision": "8a7bfbd7baeb9b578094",
+    "url": "/cardzapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "74be21e79c38aa8a6db5",
+    "url": "/cardzapp/static/css/main.1181b3ad.chunk.css"
+  },
+  {
+    "revision": "8a7bfbd7baeb9b578094",
+    "url": "/cardzapp/static/js/2.361a91c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardzapp/static/js/2.361a91c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74be21e79c38aa8a6db5",
+    "url": "/cardzapp/static/js/main.55bcd6a5.chunk.js"
+  },
+  {
+    "revision": "131cc635ac2610849e96",
+    "url": "/cardzapp/static/js/runtime-main.0044954a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cardzapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
