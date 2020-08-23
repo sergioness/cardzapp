7b6bfd770bd4d3510585620d6479849b9db26ab3
Serhii Krasnovidov <jenero77@gmail.com>
2020-08-23 21:57:56 +0300

    chore(assets): update images

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const {id, name, email} = props.info;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt={`${name} thumbnail`} src={`https://robohash.org/${id}?size=128x128`}/>
+            <img alt={`${name} thumbnail`} src={`https://robohash.org/${id}`}/>
             <h2>{name}</h2>
             <div>{email}</div>
         </div>
