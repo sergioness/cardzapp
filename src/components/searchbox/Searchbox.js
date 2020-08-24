7a6a89dcdbec0f389a1be2a089fef4fdecae6d26
Serhii Krasnovidov <jenero77@gmail.com>
2020-08-24 12:56:46 +0300

    fix(label): remove searchbox label in favor of aria-label

--- src/components/searchbox/Searchbox.js
+++ src/components/searchbox/Searchbox.js
@@ -3,13 +3,9 @@ import React from 'react';
 const Searchbox = (props) => {
     return (
         <div className='ma2'>
-            <label htmlFor='searchbox'
-                   className="f4 b pa2"
-                   style={{color:'#4cb0af'}}>
-                Search for
-            </label>
             <input type='search'
                    id='searchbox'
+                   aria-label='Search for'
                    placeholder='ex. Leanne Graham'
                    className='pa2 ba b--green bg-lightest-blue'
                    onChange={props.onSearch}>
